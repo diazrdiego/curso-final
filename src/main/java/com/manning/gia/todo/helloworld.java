8fffa6e1f3bdc9e2c8924d2bf832b135542add4d
DiegoDiaz <DiegoDiaz@ebroker.es>
2017-12-29 14:25:03 +0100

    Init files

---
+++ src/main/java/com/manning/gia/todo/helloworld.java
@@ -0,0 +1,8 @@
+package com.manning.gia.todo;
+
+class HelloWorld {
+
+	public static void main(String args[]) {
+		System.out.println("Hello world.");
+	}
+}
---
+++ src/test/java/com/manning/gia/todo/helloworldTest.java
@@ -0,0 +1,13 @@
+package com.manning.gia.todo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HelloWorldTest {
+
+    @Test
+    public void testEquals() {
+        assertEquals(20, 20);
+    }
+}
